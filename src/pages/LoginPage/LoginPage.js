a88c861b6fb0b890339ea73c06935d905827e761
Sasha Donets <sashadoneze@gmail.com>
2020-01-06 12:29:43 +0300

    Placed style for form active elements in one file

---
+++ src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,67 @@
+import React, { PureComponent } from 'react';
+import { Helmet } from 'react-helmet-async';
+import styled from 'styled-components';
+
+import { AppHeader } from '../../components/AppHeader';
+import Fieldset from '../../shared/StyledFieldset';
+import Input from '../../shared/StyledInput';
+import Section from '../../shared/StyledSection';
+import Button, { StyledButtonLink } from '../../shared/StyledButton';
+import Img from '../../shared/StyledButtonImage';
+
+import IconGoogle from '../../assets/images/iconGoogle.png';
+import IconEmail from '../../assets/images/iconEmail.png';
+
+const StyledSection = styled(Section)`
+  background-color: #f6f9fb;
+`;
+
+const H2 = styled.h2`
+  margin: 0 0 20px 0;
+`;
+
+export class LoginPage extends PureComponent {
+  state = {
+    isFormVisible: false
+  };
+
+  onEmailClick = () => {
+    this.setState({
+      isFormVisible: true
+    });
+  };
+
+  onFormSubmit = evt => {
+    evt.preventDefault();
+  };
+
+  render() {
+    return (
+      <StyledSection>
+        <Helmet>
+          <title>YouCanBook.me</title>
+        </Helmet>
+        <AppHeader />
+        <Fieldset>
+          <H2>Create your account</H2>
+          <StyledButtonLink href="/auth/google">
+            <Img src={IconGoogle} width="20px" alt="Иконка" />
+            Sign up with Google
+          </StyledButtonLink>
+
+          {this.state.isFormVisible ? (
+            <form action="register" method="POST" onSubmit={this.onFormSubmit}>
+              <Input type="text" placeholder="email@example.com" />
+              <Button type="submit">Sign up with Email</Button>
+            </form>
+          ) : (
+            <Button onClick={this.onEmailClick}>
+              <Img src={IconEmail} width="20px" alt="Icon" />
+              Sign up with Email
+            </Button>
+          )}
+        </Fieldset>
+      </StyledSection>
+    );
+  }
+}
---
+++ src/shared/StyledActiveElements.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+const CustomStyles = `
+  width: 100%;
+  height: 40px;
+  position: relative;
+  font-size: 1em;
+  font-family: MontSerrat;
+  font-weight: 300;
+  border-radius: 4px;
+  color: #333333;
+  box-shadow: none;
+  box-sizing: border-box;
+  outline: none;
+  border: none;
+  background-color: #e5e5e5;
+  margin: 10px 0;
+`;
+
+export const StyledInput = styled.input`
+  ${CustomStyles}
+  display: block;
+  border: 1.5px solid ${props => (props.invalid ? '#dc3545' : '#d7d7d7')};
+  background-color: ${props => (props.invalid ? '#ffe9e9' : '#ffffff')};
+
+  &:focus {
+    border-color: ${props => (props.invalid ? '#dc3545' : '#70a394')};
+  }
+`;
+
+export const StyledButton = styled.button`
+  ${CustomStyles}
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(0.95);
+  }
+
+  &:focus {
+    box-shadow: 0px 0px 0px 5px rgba(159, 207, 252, 0.5);
+  }
+`;
+
+export const StyledButtonLink = styled.a`
+  ${CustomStyles}
+  text-decoration: none;
+  display: block;
+  width: 100%;
+  padding: 10px 0;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(0.95);
+  }
+
+  &:focus {
+    box-shadow: 0px 0px 0px 5px rgba(159, 207, 252, 0.5);
+  }
+`;
