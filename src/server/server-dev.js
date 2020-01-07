6ce89e39c8de2ffbf997688bc953fa74e8d11c40
Sasha Donets <sashadoneze@gmail.com>
2020-01-07 09:51:06 +0300

    Change server script for connection to database and authentication

--- src/server/server-dev.js
+++ src/server/server-dev.js
@@ -1,116 +1,66 @@
+require('babel-polyfill');
+require('dotenv').config();
+
 import path from 'path';
 import express from 'express';
 import webpack from 'webpack';
+import next from 'next';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import { urlencoded, json } from 'body-parser';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-// import passport from 'passport';
-// import cors from 'cors';
+import cors from 'cors';
 import config from '../../webpack.dev.config';
+import { connectToDatabase } from './database/connection';
+import { initialiseAuthentication } from './auth';
 
-// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
 
-// const cluster = require('cluster');
-// const numCPUs = require('os').cpus().length;
+nextApp.prepare().then(async () => {
+  const app = express();
+  const compiler = webpack(config);
 
-const app = express();
-const compiler = webpack(config);
+  app.use(urlencoded({ extended: true }));
+  app.use(json());
+  app.use(cookieParser());
 
-// let User = {};
+  app.use(cors());
 
-// app.use(cors());
-// app.use(passport.initialize());
+  app.use(passport.initialize());
 
-app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath
-  })
-);
+  initialiseAuthentication(app);
 
-app.use(webpackHotMiddleware(compiler));
+  app.use(
+    webpackDevMiddleware(compiler, {
+      publicPath: config.output.publicPath
+    })
+  );
 
-// passport.serializeUser((profile, cb) => {
-//   cb(null, profile);
-// });
+  app.use(webpackHotMiddleware(compiler));
 
-// passport.deserializeUser((profile, cb) => {
-//   cb(null, profile);
-// });
+  app.get('*', (_req, res, appNext) => {
+    const indexFile = path.resolve(compiler.outputPath, 'index.html');
 
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID:
-//         '35338009580-558jj7unib9h0a1brhl7d4bupn4li2qp.apps.googleusercontent.com',
-//       clientSecret: '9NhglbsNx6CotAQTSlqykr54',
-//       callbackURL: 'http://localhost/auth/google/callback'
-//     },
-//     function(_accessToken, _refreshToken, profile, done) {
-//       User.findOrCreate({ googleId: profile.id }, function(err, user) {
-//         return done(err, user);
-//       });
-//     }
-//   )
-// );
+    compiler.outputFileSystem.readFile(indexFile, (err, result) => {
+      if (err) {
+        return appNext(err);
+      }
+      res.set('content-type', 'text/html');
+      res.send(result);
+      res.end();
+      return 0;
+    });
+  });
 
-// app.get(
-//   '/auth/google',
-//   passport.authenticate('google', {
-//     scope: ['email', 'profile']
-//   })
-// );
+  await connectToDatabase();
 
-// app.get(
-//   '/auth/google/callback',
-//   passport.authenticate('google', { failureRedirect: '/login' }),
-//   function(_req, res) {
-//     res.redirect('/');
-//   }
-// );
+  const PORT = process.env.PORT || 8080;
 
-// app.get('/user', (_req, res) => {
-//   console.log('Get user data');
-//   res.send(User);
-// });
-
-// app.get('/auth/logout', (_req, res) => {
-//   User = {};
-//   res.redirect('/');
-// });
-
-app.get('*', (_req, res, next) => {
-  const indexFile = path.resolve(compiler.outputPath, 'index.html');
-
-  compiler.outputFileSystem.readFile(indexFile, (err, result) => {
-    if (err) {
-      return next(err);
-    }
-    res.set('content-type', 'text/html');
-    res.send(result);
-    res.end();
-    return 0;
+  app.listen(PORT, () => {
+    console.log(`App listening to ${PORT}....`);
+    console.log('Press Ctrl+C to quit.');
   });
 });
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-  console.log(`App listening to ${PORT}....`);
-  console.log('Press Ctrl+C to quit.');
-});
-
-// if (cluster.isMaster) {
-//   console.log(`Master ${process.pid} is running`);
-
-//   for (let i = 0; i < numCPUs; i += 1) {
-//     cluster.fork();
-//   }
-
-//   cluster.on('exit', worker => {
-//     console.log(`worker ${worker.process.pid} died`);
-//   });
-// } else {
-//   app.listen(PORT, () => {
-//     console.log(`App listening to ${PORT}....`);
-//     console.log('Press Ctrl+C to quit.');
-//   });
-// }
