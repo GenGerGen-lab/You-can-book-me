0ff760697d81085ce813f5c2731309bae6cff372
Sasha Donets <sashadoneze@gmail.com>
2020-06-28 17:58:08 +0300

    Change section height

--- src/shared/StyledSection.js
+++ src/shared/StyledSection.js
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 
 const StyledSection = styled.section`
   width: 100%;
-  height: 100%;
+  height: auto;
+  min-height: 100%;
+  padding-bottom: 30px;
   position: absolute;
   box-sizing: border-box;
 `;
